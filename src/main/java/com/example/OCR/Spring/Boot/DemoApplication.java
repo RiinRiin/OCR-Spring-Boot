5311dae87afbe45cca1fd4f6e19a4f975feffbac
andy <jang.andy@gmail.com>
2019-08-21 16:45:56 -0400

    Added Tesseract dependency to pomp file and imported to class

--- src/main/java/com/example/OCR/Spring/Boot/DemoApplication.java
+++ src/main/java/com/example/OCR/Spring/Boot/DemoApplication.java
@@ -3,11 +3,15 @@ package com.example.OCR.Spring.Boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import  net.sourceforge.tess4j.Tesseract ;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		Tesseract tesseract =  new  Tesseract ();
 	}
 
 }
